35606b867237b20247f7c053851a9778b853bd0f
seanho <857679367@qq.com>
2024-11-10 02:44:58 +0800

    fix: some optimizations

--- src/01-algorithm/part2/03_move-zero***.ts
+++ src/01-algorithm/part2/03_move-zero***.ts
@@ -6,17 +6,17 @@
  * TC: (n^2)
  */
 function moveZero1(arr: number[]): void {
-  const length = arr.length
-  if (length === 0) return
-  let zeroLength = 0
-  for(let i = 0; i < length - zeroLength; i++) {
-    if (arr[i] === 0) {
-      arr.push(0)
-      arr.splice(i, 1)
-      i -- // 重新检查此时的i
-      zeroLength ++ // zeroLength不需要判断
+    const length = arr.length
+    if (length === 0) return
+    let zeroLength = 0
+    for (let i = 0; i < length - zeroLength; i++) {
+        if (arr[i] === 0) {
+            arr.push(0)
+            arr.splice(i, 1)
+            i-- // 重新检查此时的i
+            zeroLength++ // zeroLength不需要判断
+        }
     }
-  }
 }
 
 /**
@@ -26,35 +26,20 @@ function moveZero1(arr: number[]): void {
  * TC: O(n)
  */
 function moveZero2(arr: number[]): void {
-  const length = arr.length
+    const length = arr.length
 
-  // 定义两个指针
-  let i
-  let j = -1
+    let j = 0; // j指向下一个要放置非零元素的位置
 
-  // 循环一次即可完成功能
-  for(i = 0; i < length; i++) {
-    if(arr[i] === 0) {
-      // 找到第一个0且j还没赋值
-      if(j < 0) {
-        j = i
-      }
+    for (let i = 0; i < length; i++) {
+        if (arr[i] !== 0) {
+            [arr[j], arr[i]] = [arr[i], arr[j]];
+            j++;
+        }
     }
-
-    if(arr[i] !== 0 && j >= 0) {
-      // 如果循环到不是0的元素，则将此元素放到0的前面(此时arr[j] === 0)
-      const n = arr[i]
-      arr[i] = arr[j]
-      arr[j] = n
-
-      // 交换完 j向前一步
-      j++
-    }
-  }
 }
 
 // 功能测试
-const moveZeroArr = [1,2,0,3,0,4,0,5]
+const moveZeroArr = [1, 2, 0, 3, 0, 4, 0, 5]
 console.time('1')
 moveZero1(moveZeroArr)
 console.timeEnd('1')
--- src/01-algorithm/part2/04_most-freq-continuous-char***.ts
+++ src/01-algorithm/part2/04_most-freq-continuous-char***.ts
@@ -1,7 +1,7 @@
 
 interface IRes {
-  char: string,
-  length: number
+    char: string,
+    length: number
 }
 
 /**
@@ -9,42 +9,31 @@ interface IRes {
  * @param str
  * @returns
  */
-function findContinuousChar1(str: string): IRes {
-  const res: IRes = {
-    char: '',
-    length: 0
-  }
-  const length = res.length
-  if (length === 0) return res
-
-  let tempLength = 0
-  for (let i = 0; i < length; i++) {
-    // 记得重置
-    tempLength = 0
-
-    for (let j = i; j < length; j++) {
-      if(str[i] === str[j]) {
-        // 相等则记录
-        tempLength++
-      }
-
-      if (str[i] !== str[j] || j === length - 1) {
-        // 不相等 或者走到最后一个元素 去判断最大值
-        if(tempLength > res.length) {
-          res.char = str[i]
-          res.length = tempLength
-        }
-
-        if (i < length - 1) { // 如果i不指向最后一个
-          i = j - 1 // 跳步
-        }
-
-        break
-      }
+function findLongestContinuousChar(str: string): IRes {
+    const res: IRes = {
+        char: '',
+        length: 0
     }
-  }
+    const length = res.length
+    for (let i = 0; i < length; i++) {
+        // 记得重置
+        let tempLength = 0
+        for (let j = i + 1; j < length; j++) {
+            if (str[i] === str[j]) {
+                // 相等则记录
+                tempLength++
+            } else {
+                break
+            }
+        }
+        if (tempLength > res.length) {
+            res.char = str[i]
+            res.length = tempLength
+        }
 
-  return res
+    }
+
+    return res
 }
 
 /**
@@ -52,53 +41,47 @@ function findContinuousChar1(str: string): IRes {
  * @param str
  * @returns
  */
-function findContinuousChar2(str: string): IRes {
-  const res = {
-    char: '',
-    length: 0
-  }
-  const length = str.length
-  if (length === 0) return res
-  let tempLength = 0
-
-  // 定义两个双指针
-  let i = 0
-  let j = 0
-
-  for(; i < length; i++) {
-    if (str[i] === str[j]) {
-      // 如果相等则记录+1
-      tempLength++
+function findLongestContinuousChar2(str: string): IRes {
+    const res = {
+        char: '',
+        length: 0
     }
-    if (str[i] !== str[j] || i === length - 1) {
-      // 如果不相等则比较长度,大于则替换res的内容
-      if (tempLength > res.length) {
-        res.length = tempLength
-        res.char = str[i]
-      }
-      // 任何情况的不相等都要重置tempLength
-      tempLength = 0
+    const length = str.length
+    if (length === 0) return res
+    let tempLength = 0
 
-      if (i < length - 1) {
-        // 如果不相等且没有循环完且tempLength没有比现有的res.length长，则
-        j = i
-        i-- // 因为最外层循环有个i++ 所以这里i-- (细节！)
-      }
+    // 定义两个双指针
+    let i = 0
+    let j = 0
+
+    for (; i < length; i++) {
+        if (str[i] === str[j]) {
+            // 如果相等则记录+1
+            tempLength++
+        } else {
+            // 如果不相等则比较长度,大于则替换res的内容
+            if (tempLength > res.length) {
+                res.length = tempLength
+                res.char = str[i]
+            }
+            // 任何情况的不相等都要重置tempLength
+            tempLength = 1
+            j = i
+        }
     }
-  }
 
-  return res
+    return res
 }
 
 // 功能测试
 const str1 = 'aakkkkcccoooooooo'
 
-console.time('findContinuousChar1')
-findContinuousChar1(str1)
-console.timeEnd('findContinuousChar1')
+console.time('findLongestContinuousChar')
+findLongestContinuousChar(str1)
+console.timeEnd('findLongestContinuousChar')
 
-console.time('findContinuousChar1')
-findContinuousChar2(str1)
-console.timeEnd('findContinuousChar1')
+console.time('findLongestContinuousChar2')
+findLongestContinuousChar2(str1)
+console.timeEnd('findLongestContinuousChar2')
 
 // 经过时间测试发现 两个时间复杂度差不多O(n)
